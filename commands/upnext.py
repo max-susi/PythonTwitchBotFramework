5c9d0b10a16509e3208a9dd1580291108219c36a
Susi <alexandermax.rickert@signal-iduna.de>
2021-05-01 13:53:43 +0200

    fixed path to redeem bot

--- commands/upnext.py
+++ commands/upnext.py
@@ -4,5 +4,5 @@ import os
 @Command('upnext')
 async def cmd_function(msg, *args):
     #print(getNameOfTopEntryInList.getIdForList("UP NEXT"))
-    next = os.popen("cd  commands/redeem_bot && python getNameOfTopEntryInList.py").read()
+    next = os.popen("cd  commands/redeem-bot && python3 getNameOfTopEntryInList.py").read()
     await msg.reply("upnext: " + next)
